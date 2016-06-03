24e5c2816c2b248b86c628714bde78d2458a7056
Felix SOEDJEDE <soefelix@gmail.com>
2016-06-03 12:24:34 +0200

    Correction du test de recette
    Test : Ajout d'une ressource secondaire

--- spec/features/ajouter_une_ressource_secondaire.rb
+++ spec/features/ajouter_une_ressource_secondaire.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-feature 'Enrichir son profil avec une compétence / centre d intérêt existant' do
+feature 'Enrichir son profil avec des ressources secondaires' do
 
-  scenario 'ajouter par le bouton plus' do
+  scenario 'ajouter une référence' do
       visit $home_page
       click_on 'Se connecter'
       log_in_as 'alice', 'lapinblanc'
@@ -10,8 +10,8 @@ feature 'Enrichir son profil avec une compétence / centre d intérêt existant'
       click_on_link 'Baptiste BAINIER'
       toggle_edit
       click_plus_sign_next_to 'local-resource'
-      fill_in 'Nom : ', :with => 'Guillaume DOYEN(referent)'
-      fill_in 'Adresse : ', :with => 'http://guillaume-doyen.fr'
+      fill_in 'attributename', :with => 'Guillaume DOYEN(referent)'
+      fill_in 'attributevalue', :with => 'http://guillaume-doyen.fr'
       click_on 'Référencer'
     end
 end
